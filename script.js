100248069e42320355d02f0f70851592da36b6f1
faustine236 <541231157@student.smktelkom-pwt.sch.id>
2024-08-26 15:39:07 +0700

    otin_login

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-document.getElementById("LoginForm").addEventListener('submit',function(event){
-event.preventDefault();
+document.getElementById('LoginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-const correctUsername = 'otin';
-const correctPassword = 'otin';
+    const correctUsername = 'otin';
+    const correctPassword = 'otin';
 
-const username = document.getElementById('username').value;
-const password = document.getElementById('password').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-if(username=== correctUsername && password === correctPassword){
-    alert("login berhasil");
-    window.location.href = index.html;
-}else{
-    alert("username atau password tidak sesuai");
-}
+    if (username === correctUsername && password === correctPassword) {
+        alert("Login Berhasil");
+        window.location.href = 'index.html';
+    } else {
+        alert("Username atau password tidak sesuai");
+    }
 });
-
